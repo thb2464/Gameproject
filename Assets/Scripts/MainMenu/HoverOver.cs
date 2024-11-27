94098d0c9950626c33fc70214e2d470cade39132
Anh Tuan <therealpaterra22@gmail.com>
2024-11-27 19:50:12 +0700

    MainMenu

---
+++ Assets/Scripts/MainMenu/HoverOver.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject HoverPanel;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HoverPanel.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HoverPanel.SetActive(false);
+    }
+}
